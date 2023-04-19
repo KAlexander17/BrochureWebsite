3577470882067e3e792bd609fc75f3c8c42f08b4
Kyle Alexander <kbalexander17@yahoo.com>
2023-04-19 18:18:04 -0400

    MenuAdditions

--- src/components/pages/Menu.js
+++ src/components/pages/Menu.js
@@ -1,8 +1,21 @@
 import React from "react";
+import '../../App.css';
+import './Menu.css';
 
 function Menu () {
     return (
-        <h1>Test</h1>
+        <>
+            <h1>Our Menu</h1>
+            <div class='container'>
+                <div class='image'>
+                    <img src='https://www.allrecipes.com/thmb/ASRzxoRrPoMLQEpczFvU7osJNF4=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/21353-italian-spaghetti-sauce-with-meatballs-2x1-141-cedbb650b4264576ab923c91215ce7fc.jpg' />
+                </div>
+                <div class='text'>
+                    <h2>Spaghetti</h2>
+                    <p>Placeholder</p>
+                </div>
+            </div>
+        </>
     )
 };
 
