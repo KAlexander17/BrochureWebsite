e843b53c9d4ef2fd22a3cb2c6242392d99162e4a
Kyle Alexander <kbalexander17@yahoo.com>
2023-04-24 18:46:14 -0400

    About us page added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/pages/Home';
 import Menu from './components/pages/Menu';
+import About from './components/pages/About';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='/menu' element={<Menu/>} />
+          <Route path='/about' element={<About/>} />
         </Routes>
       </Router>
     </>
---
+++ src/components/pages/About.js
@@ -0,0 +1,24 @@
+import React from "react";
+import '../../App.css';
+import './About.css';
+
+function About() {
+    return (
+        <>
+            <div class='container'>
+                <h1>About our family</h1>
+                <p>
+                    Luigi's started in 2001 and has been serving only the best for authentic Italian cuisine
+                    since we opened. All of our ingredients are prepared here in house and are as fresh as fresh
+                    can get!
+                </p>
+                <p>
+                    Luigi's was started by Luigi Ricci after he immigrated to the United States from Italy back
+                    in 1998, and he wanted to bring the taste of Italy to the US! 
+                </p>
+            </div>
+        </>
+    )
+};
+
+export default About;
